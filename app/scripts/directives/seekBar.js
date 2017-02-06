7c9efc9582d2657ba1cffbd4e107e58e4b718245
gt5955a <garry.tamhuang@gmail.com>
2017-02-06 17:49:31 -0500

    Assignment 9: Completed

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -1,5 +1,11 @@
 (function() {
     function seekBar($document) {
+        
+        /**
+        * @function calculatePercent
+        * @desc Calculates percentage of seek bar length
+        * @param {Object} seekBar {Object} event
+        */
         var calculatePercent = function(seekBar, event) {
             var offsetX = event.pageX - seekBar.offset().left;
             var seekBarWidth = seekBar.width();
@@ -20,6 +26,10 @@
                 
                 var seekBar = $(element);
                 
+                /**
+                * @function percentString
+                * @desc Converts percentage into string
+                */
                 var percentString = function() {
                     var value = scope.value;
                     var max = scope.max;
@@ -27,15 +37,35 @@
                     return percent + "%";
                 };
                 
+                /**
+                * @function fillStyle
+                * @desc Returns width of bar based on percent
+                */
                 scope.fillStyle = function() {
                     return {width: percentString()};
                 };
                 
+                /**
+                * @function thumbStyle
+                * @desc Updates position of thumb
+                */
+                scope.thumbStyle = function() {
+                    return {left: percentString()};
+                };
+                
+                /**
+                * @function onClickSeekBar
+                * @desc Updates seek bar value based on bar width and location of click
+                */
                 scope.onClickSeekBar = function(event) {
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
                 };
                 
+                /**
+                * @function trackThumb
+                * @desc Updates seek bar value when thumb is dragged
+                */
                 scope.trackThumb = function() {
                     $document.bind('mousemove.thumb', function(event) {
                         var percent = calculatePercent(seekBar, event);
